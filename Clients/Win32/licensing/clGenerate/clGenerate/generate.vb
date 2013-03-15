0ca09daf83e00c49053809a796219147e3f25f07
ThomasMW <ThomasM@emerge-it.co.uk>
2013-03-15 21:51:01 +0000

    Implementation example & command line keygen

---
+++ Clients/Win32/licensing/clGenerate/clGenerate/generate.vb
@@ -0,0 +1,24 @@
+﻿Imports licensing
+
+Module generate
+
+    Sub Main()
+        Console.Write("Assembly name: ")
+        Dim assembly As String = Console.ReadLine()
+        Console.Write("MAC address (press enter to use this machine's): ")
+        Dim mac As String = Console.ReadLine()
+
+        If mac = "" Then
+            mac = System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces()(0).GetPhysicalAddress.ToString
+        ElseIf mac.Length < 12 Then
+            Console.WriteLine("Invalid mac address!")
+            Exit Sub
+        End If
+
+        Dim gn As New generator(assembly, mac)
+        Console.WriteLine(gn.license)
+        Console.ReadLine()
+
+    End Sub
+
+End Module
