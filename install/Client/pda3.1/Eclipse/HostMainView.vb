066a907b0efd972c84033591e6cd67e1a22f8c44
Simon Barnett <simonbarnett@emerge-it.co.uk>
2013-10-21 15:07:42 +0100

    Eclipse PDA 3.2

---
+++ install/Client/pda3.1/Eclipse/HostMainView.vb
@@ -0,0 +1,195 @@
+﻿Imports PrioritySFDC
+Imports PriorityMobile
+Imports System.IO
+Imports System.Runtime.InteropServices
+Imports System.Xml
+Imports System.Reflection
+
+Public Class HostMainView
+
+#Region "Private Properties"
+
+    Private ReadOnly Property Paths() As String()
+        Get
+            Dim ret() As String = { _
+                            "report/detail/malfunction", _
+                            "report/detail/resolution", _
+                            "report/detail/repair", _
+                            "report/signature/image", _
+                            "report/signature/print", _
+                            "report/times" _
+                        }
+            Return ret
+        End Get
+    End Property
+
+    Private _ShowTaskbar As Boolean = True
+    Private Property ShowTaskbar() As Boolean
+        Get
+            Return _ShowTaskbar
+        End Get
+        Set(ByVal value As Boolean)
+            Dim h As IntPtr = FindWindow("HHTaskBar", "")
+            Select Case value
+                Case True
+                    ShowWindow(h, &H1)
+                    EnableWindow(h, True)
+                Case False
+                    ShowWindow(h, &H0)
+                    EnableWindow(h, False)
+                    Me.Focus()
+            End Select
+            _ShowTaskbar = value
+            Me.ControlBox = False
+        End Set
+    End Property
+
+#End Region
+
+#Region "initialisation and finalisation"
+
+    Private Sub init(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Try
+            With MainView
+                .LoadUE(ue)
+                .xf = New xmlForms( _
+                    .ue, _
+                    ue("forms"), _
+                    ue("calls"), _
+                    ue("lookup"), _
+                    ue("statusrules") _
+                )
+                .xf.Printer = New btZebra.LabelPrinter( _
+                    New Point(300, 300), _
+                    New Size(576, 0), _
+                    .ue.AppPath & "\prnimg\" _
+                )
+                .LoadViews()
+            End With
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.OkOnly + MsgBoxStyle.Critical, "Fatal Error.")
+            ue.Log(ex.Message)
+            Application.Exit()
+            Exit Sub
+        End Try
+
+        Cursor.Current = Cursors.Default
+        Me.ShowTaskbar = False
+
+    End Sub
+
+    Private Sub frmMain_Closing(ByVal sender As Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
+        ShowTaskbar = True
+        Application.Exit()
+    End Sub
+
+    Private Sub frmMain_Activated(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Activated
+        ShowTaskbar = False
+        'Me.WindowState = FormWindowState.Maximized
+    End Sub
+
+    Private Sub frmMain_Deactivate(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Deactivate, MyBase.Disposed
+        ShowTaskbar = True
+        'Me.WindowState = FormWindowState.Normal
+    End Sub
+
+#End Region
+
+#Region "Main View Event Handlers"
+
+    Private Sub hLoadiView(ByVal ControlName As String, ByRef view As iView, ByRef thisForm As xForm) Handles MainView.LoadiView
+        Dim f As Boolean = False
+        With ue
+            If Not IsNothing(.CustomHandler) Then
+                Try
+                    Dim oType As System.Type = .CustomHandler.GetType(String.Format("pdaHandler.ctrl_{0}", ControlName.Replace(".vb", "")))
+                    view = Activator.CreateInstance(oType)
+                Catch EX As Exception
+                    view = New ctrl_DataGrid
+                End Try
+            End If
+        End With
+        view.SetForm(thisForm)
+    End Sub
+
+    Private Sub hSyncEvent(ByVal NodeType As String, ByVal EventType As eSyncEventType, ByRef DataXML As OfflineXML) Handles MainView.SyncEvent
+
+        With DataXML
+            Select Case EventType
+                Case eSyncEventType.BeginDownload
+                    'myProgressBar.txt_File.Text = "Download: " & .FileURL
+                    'myProgressBar.txt_Action.Text = ""
+
+                Case eSyncEventType.EndDownload
+                    'myProgressBar.txt_File.Text = ""
+                    'myProgressBar.txt_Action.Text = ""
+
+                Case eSyncEventType.NewNode
+                    Select Case NodeType.ToLower
+                        Case "servicecall"
+                            'myProgressBar.txt_File.Text = "Sync: " & .FileURL
+                            'myProgressBar.txt_Action.Text = "Adding call: " & .CurrentNode.SelectSingleNode("callnumber").InnerText
+                            .MakePaths(Paths)
+
+                        Case "part"
+                            'myProgressBar.txt_File.Text = "Sync: " & .FileURL
+                            'myProgressBar.txt_Action.Text = "Adding part: " & .CurrentNode.SelectSingleNode("name").InnerText
+
+                    End Select
+
+                Case eSyncEventType.EditNode
+                    Select Case NodeType.ToLower
+                        Case "servicecall"
+                            'myProgressBar.txt_File.Text = "Sync: " & .FileURL
+                            'myProgressBar.txt_Action.Text = "Edit call: " & .CurrentNode.SelectSingleNode("callnumber").InnerText
+                            .MakePaths(Paths)
+
+                        Case "part"
+                            'myProgressBar.txt_File.Text = "Sync: " & .FileURL
+                            'myProgressBar.txt_Action.Text = "Edit part: " & .CurrentNode.SelectSingleNode("name").InnerText
+
+                    End Select
+
+                Case eSyncEventType.DeleteNode
+                    Select Case NodeType.ToLower
+                        Case "servicecall"
+                            'myProgressBar.txt_File.Text = "Sync: " & .FileURL
+                            'myProgressBar.txt_Action.Text = "Remove call: " & .CurrentNode.SelectSingleNode("callnumber").InnerText
+                        Case "part"
+                            'myProgressBar.txt_File.Text = "Sync: " & .FileURL
+                            'myProgressBar.txt_Action.Text = "Remove part: " & .CurrentNode.SelectSingleNode("name").InnerText
+                    End Select
+
+                Case eSyncEventType.EndSync
+                    'myProgressBar.txt_File.Text = ""
+                    'myProgressBar.txt_Action.Text = ""
+                    Me.ShowTaskbar = False
+                    Me.Focus()
+
+            End Select
+        End With
+    End Sub
+
+    Private Sub hSetForm() Handles MainView.SetForm
+        With Me
+            .Menu = mainMenu1
+            .ControlBox = False
+            .FormBorderStyle = Windows.Forms.FormBorderStyle.None
+            .WindowState = FormWindowState.Maximized
+            .ShowTaskbar = False
+        End With
+    End Sub
+
+    Private Sub hCloseForm() Handles MainView.BeginClose
+        Me.ControlBox = Not (Me.ControlBox)
+    End Sub
+
+    Private Sub hSetTaskbar(ByVal Visible As Boolean)
+        ShowTaskbar = Visible
+    End Sub
+
+#End Region
+
+End Class
---
+++ install/Client/pda3.1/Eclipse/cHandler/dialogs/dlgOnCall.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Public Class dlgOnCall
+    Inherits PriorityMobile.UserDialog
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel
+        Me.btnOK = New System.Windows.Forms.Button
+        Me.btnCancel = New System.Windows.Forms.Button
+        Me.txtcallnumber = New System.Windows.Forms.TextBox
+        Me.lblOnCall = New System.Windows.Forms.Label
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.btnOK)
+        Me.Panel1.Controls.Add(Me.btnCancel)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel1.Location = New System.Drawing.Point(0, 218)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(246, 45)
+        '
+        'btnOK
+        '
+        Me.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.btnOK.Dock = System.Windows.Forms.DockStyle.Left
+        Me.btnOK.Location = New System.Drawing.Point(0, 0)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(120, 45)
+        Me.btnOK.TabIndex = 8
+        Me.btnOK.Text = "Open Call"
+        '
+        'btnCancel
+        '
+        Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancel.Dock = System.Windows.Forms.DockStyle.Right
+        Me.btnCancel.Location = New System.Drawing.Point(133, 0)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(113, 45)
+        Me.btnCancel.TabIndex = 7
+        Me.btnCancel.Text = "Cancel"
+        '
+        'txtcallnumber
+        '
+        Me.txtcallnumber.Location = New System.Drawing.Point(146, 191)
+        Me.txtcallnumber.Name = "txtcallnumber"
+        Me.txtcallnumber.Size = New System.Drawing.Size(100, 21)
+        Me.txtcallnumber.TabIndex = 9
+        Me.txtcallnumber.Visible = False
+        '
+        'lblOnCall
+        '
+        Me.lblOnCall.Location = New System.Drawing.Point(20, 20)
+        Me.lblOnCall.Name = "lblOnCall"
+        Me.lblOnCall.Size = New System.Drawing.Size(208, 80)
+        '
+        'dlgOnCall
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.Controls.Add(Me.lblOnCall)
+        Me.Controls.Add(Me.txtcallnumber)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "dlgOnCall"
+        Me.Size = New System.Drawing.Size(246, 263)
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents btnOK As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+    Friend WithEvents txtcallnumber As System.Windows.Forms.TextBox
+    Friend WithEvents lblOnCall As System.Windows.Forms.Label
+
+End Class
