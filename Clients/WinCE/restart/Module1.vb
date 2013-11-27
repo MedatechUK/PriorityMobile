bd3daad0fe82b5030f7def6703bf9f2034f511c4
Simon Barnett <simonbarnett@emerge-it.co.uk>
2013-11-27 14:00:23 +0000

    WinCE Restart

---
+++ Clients/WinCE/restart/Module1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Runtime.InteropServices
+Module Module1
+
+    Const POWER_STATE_RESET As Integer = &H800000
+    <DllImport("coredll")> _
+    Private Function SetSystemPowerState(ByVal psState As String, ByVal StateFlags As Integer, ByVal Options As Integer) As Integer
+    End Function
+
+    Sub Main()
+        Dim StartTime As Date = Now
+        Do
+            If DateDiff(DateInterval.Hour, StartTime, Now) > 0 Then
+                If Now.Minute = 30 And Now.Hour = 16 Then
+                    SetSystemPowerState(Nothing, POWER_STATE_RESET, 0)
+                End If
+            End If
+            Threading.Thread.Sleep(500)
+        Loop
+    End Sub
+
+End Module
---
+++ Clients/WinCE/restart/My Project/AssemblyInfo.vb
@@ -0,0 +1,36 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("restart")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("restart")> 
+<Assembly: AssemblyCopyright("Copyright ©  2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: CLSCompliant(True)> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("55e327e9-cf25-4a6f-8968-c61ce2f450ee")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
