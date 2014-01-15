7117f518bdb86e750e27d12801a001e0eec0123d
ThomasMW <ThomasM@emerge-it.co.uk>
2014-01-15 12:25:32 +0000

    initial commit [broken build - WIP]

---
+++ Clients/Win32/emgPDF2/emgPDF2/Module1.vb
@@ -0,0 +1,31 @@
+﻿Imports HiQPdf
+Imports HtmlAgilityPack
+
+Module Module1
+
+    Sub main()
+        HtmlNode.ElementsFlags.Remove("form")
+        PDFShared.PDFParams = New PdfParameters(System.Environment.GetCommandLineArgs())
+        Dim Docs As New emgPDFDoc(PDFShared.PDFParams)
+
+    End Sub
+
+    Public Sub ParseArgs()
+
+    End Sub
+
+#Region "parse html into separate documents:: input single html file"
+
+#End Region
+
+#Region "handle headers, footers per document html:: input html as strings"
+
+#End Region
+
+#Region "handle multiple document merge- pagination?:: input collection of htmls?"
+
+#End Region
+
+
+End Module
+
